a9ee2262f0e5660440113fe28ccc22f6c07cd357
NuclearCosmo <mac.macivor@uoit.net>
2021-05-05 05:32:58 -0400

    Pulling and grapple now follows the mouse

--- SCUBA/Assets/playerBehavior.cs
+++ SCUBA/Assets/playerBehavior.cs
@@ -23,6 +23,9 @@ public class playerBehavior : MonoBehaviour
     [Range(0, 2000)]
     public float grapplePower = 1;
 
+    Vector2 mousePosition;
+    Vector3 maxDistance;
+
     public int dir = 0;
 
     //0  1  2
@@ -31,8 +34,16 @@ public class playerBehavior : MonoBehaviour
 
 
     private bool grapple = true;
+    private bool pull = true;
     private bool inWater = false;
     RaycastHit watRay; //= Physics.SphereCastAll(Vector3.forward, 0, Vector3.forward);
+    private Vector3 angle;
+    bool pullingObject = false;
+    GameObject pullingOBJ;
+    Vector3 currentPullPos;
+    Vector3 previousPullPos;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -44,6 +55,7 @@ public class playerBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Physics.SphereCast(player.transform.position, 0.1f, Vector3.back, out watRay, 0.2f);
         if (watRay.collider != null)
         {
@@ -71,11 +83,21 @@ public class playerBehavior : MonoBehaviour
                 player.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpStrength);
             }
 
-            if (Input.GetKey(KeyCode.LeftShift) && grapple)
-            {
-                grapple = false;
-                ropeDurationAt = 0;
-            }
+        }
+        
+        if (Input.GetMouseButton(0) && grapple && pull)
+        {
+            grapple = false;
+            ropeDurationAt = 0;
+            angle = Vector3.Normalize(new Vector3(mousePosition.x, mousePosition.y, player.transform.position.z) - player.transform.position);
+            maxDistance = player.transform.position + ropeRange * angle;
+        }
+        else if (Input.GetMouseButton(1) && grapple && pull)
+        {
+            pull = false;
+            ropeDurationAt = 0;
+            angle = Vector3.Normalize(new Vector3(mousePosition.x, mousePosition.y, player.transform.position.z) - player.transform.position);
+            maxDistance = player.transform.position + ropeRange * angle;
         }
 
 
@@ -108,8 +130,6 @@ public class playerBehavior : MonoBehaviour
             if (grapple)
                 dir += 1;
         }
-        
-        drawLine();
 
         if (!grapple)
         {
@@ -117,94 +137,16 @@ public class playerBehavior : MonoBehaviour
             Vector3[] newVerts = { new Vector3(), new Vector3()};
             RaycastHit ray; //= Physics.SphereCastAll(Vector3.forward, 0, Vector3.forward);
             Physics.SphereCast(transform.position, 0.13f, transform.forward, out ray, 0);
-            switch (dir)
-            {
-                case 0:
-                    newVerts[0] = player.transform.position;
-                    newVerts[1] = Vector3.Lerp(player.transform.position, player.transform.position + ropeRange * new Vector3(-0.707f, 0.707f, 0), ropeDurationAt / ropeDuration);
-                    Physics.SphereCast(player.transform.position, 0.13f, new Vector3(-0.707f, 0.707f, 0), out ray, Vector2.Distance(newVerts[0], newVerts[1]));
-                    break;
-                case 1:
-                    newVerts[0] = player.transform.position;
-                    newVerts[1] = Vector3.Lerp(player.transform.position, player.transform.position + ropeRange * new Vector3(0, 1, 0), ropeDurationAt / ropeDuration);
-                    Physics.SphereCast(player.transform.position, 0.13f, new Vector3(0, 1, 0), out ray, Vector2.Distance(newVerts[0], newVerts[1]));
-                    break;
-                case 2:
-                    newVerts[0] = player.transform.position;
-                    newVerts[1] = Vector3.Lerp(player.transform.position, player.transform.position + ropeRange * new Vector3(0.707f, 0.707f, 0), ropeDurationAt / ropeDuration);
-                    Physics.SphereCast(player.transform.position, 0.13f, new Vector3(0.707f, 0.707f, 0), out ray, Vector2.Distance(newVerts[0], newVerts[1]));
-                    break;
-                case 3:
-                    newVerts[0] = player.transform.position;
-                    newVerts[1] = Vector3.Lerp(player.transform.position, player.transform.position + ropeRange * new Vector3(-1, 0, 0), ropeDurationAt / ropeDuration);
-                    Physics.SphereCast(player.transform.position, 0.13f, new Vector3(-1, 0, 0), out ray, Vector2.Distance(newVerts[0], newVerts[1]));
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    newVerts[0] = player.transform.position;
-                    newVerts[1] = Vector3.Lerp(player.transform.position, player.transform.position + ropeRange * new Vector3(1, 0, 0), ropeDurationAt / ropeDuration);
-                    Physics.SphereCast(player.transform.position, 0.13f, new Vector3(-1, 0, 0), out ray, Vector2.Distance(newVerts[0], newVerts[1]));
-                    break;
-                case 6:
-                    newVerts[0] = player.transform.position;
-                    newVerts[1] = Vector3.Lerp(player.transform.position, player.transform.position + ropeRange * new Vector3(-0.707f, -0.707f, 0), ropeDurationAt / ropeDuration);
-                    Physics.SphereCast(player.transform.position, 0.13f, new Vector3(-0.707f, -0.707f, 0), out ray, Vector2.Distance(newVerts[0], newVerts[1]));
-                    break;
-                case 7:
-                    newVerts[0] = player.transform.position;
-                    newVerts[1] = Vector3.Lerp(player.transform.position, player.transform.position + ropeRange * new Vector3(0, -1, 0), ropeDurationAt / ropeDuration);
-                    Physics.SphereCast(player.transform.position, 0.13f, new Vector3(0, -1, 0), out ray, Vector2.Distance(newVerts[0], newVerts[1]));
-                    break;
-                case 8:
-                    newVerts[0] = player.transform.position;
-                    newVerts[1] = Vector3.Lerp(player.transform.position, player.transform.position + ropeRange * new Vector3(0.707f, -0.707f, 0), ropeDurationAt / ropeDuration);
-                    Physics.SphereCast(player.transform.position, 0.13f, new Vector3(0.707f, -0.707f, 0), out ray, Vector2.Distance(newVerts[0], newVerts[1]));
-                    break;
-            }
+            newVerts[0] = player.transform.position;
+            newVerts[1] = Vector3.Lerp(player.transform.position, maxDistance, ropeDurationAt / ropeDuration);
+            Physics.SphereCast(player.transform.position, 0.13f, angle, out ray, Vector2.Distance(newVerts[0], newVerts[1]));
+            
             rope.GetComponent<LineRenderer>().SetPositions(newVerts);
             
             if (ray.collider != null)
             {
 
-                switch (dir)
-                {
-                    case 0:
-                        player.GetComponent<Rigidbody>().AddForce(new Vector3(-0.707f, 0.707f, 0) * grapplePower);
-                        break;
-                    case 1:
-                        player.GetComponent<Rigidbody>().AddForce(new Vector3(0, 1, 0) * grapplePower);
-
-                        break;
-                    case 2:
-                        player.GetComponent<Rigidbody>().AddForce(new Vector3(0.707f, 0.707f, 0) * grapplePower);
-
-                        break;
-                    case 3:
-                        player.GetComponent<Rigidbody>().AddForce(new Vector3(-1, 0, 0) * grapplePower);
-
-                        break;
-                    case 4:
-                        ;
-                        break;
-                    case 5:
-                        player.GetComponent<Rigidbody>().AddForce(new Vector3(1, 0, 0) * grapplePower);
-
-                        break;
-                    case 6:
-                        player.GetComponent<Rigidbody>().AddForce(new Vector3(-0.707f, -0.707f, 0) * grapplePower);
-
-                        break;
-                    case 7:
-                        player.GetComponent<Rigidbody>().AddForce(new Vector3(0, -1, 0) * grapplePower);
-
-                        break;
-                    case 8:
-                        player.GetComponent<Rigidbody>().AddForce(new Vector3(0.707f, -0.707f, 0) * grapplePower);
-
-                        break;
-
-                }
+                player.GetComponent<Rigidbody>().AddForce(angle * grapplePower);
 
                 newVerts[0] = Vector3.zero;
                 newVerts[1] = Vector3.zero;
@@ -224,7 +166,90 @@ public class playerBehavior : MonoBehaviour
             }
 
         }
+        else if (!pull)
+        {
+            Vector3[] newVerts = { new Vector3(), new Vector3() };
+            RaycastHit ray; //= Physics.SphereCastAll(Vector3.forward, 0, Vector3.forward);
+            Physics.SphereCast(Vector3.zero, 0, Vector3.zero, out ray);
+            if (!pullingObject)
+            {
+                ropeDurationAt += Time.deltaTime;
+                Physics.SphereCast(transform.position, 0.13f, transform.forward, out ray, 0);
+                newVerts[0] = player.transform.position;
+                newVerts[1] = Vector3.Lerp(player.transform.position, maxDistance, ropeDurationAt / ropeDuration);
+                Physics.SphereCast(player.transform.position, 0.13f, angle, out ray, Vector2.Distance(newVerts[0], newVerts[1]));
+                rope.GetComponent<LineRenderer>().SetPositions(newVerts);
+            }
+            else
+            {
+                ropeDurationAt -= Time.deltaTime;
+                Physics.SphereCast(transform.position, 0.13f, transform.forward, out ray, 0);
+                newVerts[0] = player.transform.position;
+                newVerts[1] = Vector3.Lerp(player.transform.position, maxDistance, ropeDurationAt / ropeDuration);
+                Physics.SphereCast(player.transform.position, 0.13f, angle, out ray, Vector2.Distance(newVerts[0], newVerts[1]));
+                rope.GetComponent<LineRenderer>().SetPositions(newVerts);
 
+                currentPullPos = newVerts[1];
+
+                Vector3 distance = currentPullPos - previousPullPos;
+
+                pullingOBJ.transform.position = pullingOBJ.transform.position + distance;
+
+                previousPullPos = newVerts[1];
+
+
+            }
+
+            if (ray.collider != null)
+            {
+                if (ray.collider.tag == "Pullable")
+                {
+                    pullingOBJ = ray.collider.gameObject;
+                    pullingObject = true;
+                    currentPullPos = newVerts[1];
+                    previousPullPos = newVerts[1];
+                }
+                else
+                {
+                    //player.GetComponent<Rigidbody>().AddForce(angle * grapplePower);
+
+                    newVerts[0] = Vector3.zero;
+                    newVerts[1] = Vector3.zero;
+                    rope.GetComponent<LineRenderer>().SetPositions(newVerts);
+                    pull = true;
+                    ropeDurationAt = 0;
+                }
+            }
+
+
+
+            if (!(ropeDurationAt < ropeDuration))
+            {
+                newVerts[0] = Vector3.zero;
+                newVerts[1] = Vector3.zero;
+                rope.GetComponent<LineRenderer>().SetPositions(newVerts);
+                pull = true;
+                currentPullPos = Vector3.zero;
+                previousPullPos = Vector3.zero;
+                pullingObject = false;
+                pullingOBJ = null;
+                ropeDurationAt = 0;
+
+            }
+            else if (ropeDurationAt < 0)
+            {
+                newVerts[0] = Vector3.zero;
+                newVerts[1] = Vector3.zero;
+                rope.GetComponent<LineRenderer>().SetPositions(newVerts);
+                pull = true;
+                pullingObject = false;
+                currentPullPos = Vector3.zero;
+                previousPullPos = Vector3.zero;
+                pullingOBJ = null;
+                ropeDurationAt = 0;
+
+            }
+        }
     }
 
     private GameObject createLine(Vector3[] verts)
@@ -245,50 +270,6 @@ public class playerBehavior : MonoBehaviour
         return didHit;
     }
 
-    public void drawLine()
-    {
-        switch (dir)
-        {
-            //0  1  2
-            //3  4  5
-            //6  7  8
-            case 0:
-                
-                Debug.DrawLine(player.transform.position, player.transform.position + ropeRange * new Vector3(-0.707f, 0.707f, 0));
-
-                break;
-            case 1:
-                Debug.DrawLine(player.transform.position, player.transform.position + ropeRange * new Vector3(0, 1, 0));
-
-                break;
-            case 2:
-                Debug.DrawLine(player.transform.position, player.transform.position + ropeRange * new Vector3(0.707f, 0.707f, 0));
-
-                break;
-            case 3:
-                Debug.DrawLine(player.transform.position, player.transform.position + ropeRange * new Vector3(-1, 0, 0));
-
-                break;
-            case 4:
-                ;
-                break;
-            case 5:
-                Debug.DrawLine(player.transform.position, player.transform.position + ropeRange * new Vector3(1, 0, 0));
-
-                break;
-            case 6:
-                Debug.DrawLine(player.transform.position, player.transform.position + ropeRange * new Vector3(-0.707f, -0.707f, 0));
-
-                break;
-            case 7:
-                Debug.DrawLine(player.transform.position, player.transform.position + ropeRange * new Vector3(0, -1, 0));
-
-                break;
-            case 8:
-                Debug.DrawLine(player.transform.position, player.transform.position + ropeRange * new Vector3(0.707f, -0.707f, 0));
-
-                break;
-        }
-    }
+    
 
 }
